803df1848acb26bc0dcfaf4883d22817f6f4484a
paresh-2002 <pareshparesh.ay99099@gmail.com>
2024-09-12 15:28:23 +0530

    Add style

--- 04bgChanger/src/Components/Button.jsx
+++ 04bgChanger/src/Components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Button({setColors, color}) {
   return (
-    <button className={`outline-none shadow-lg px-4 py-1 rounded-full 'text-black' `} style={{backgroundColor:color}} onClick={() =>setColors(color)}>{color}</button>
+    <button className='outline-none shadow-lg px-4 py-1 rounded-full' style={{backgroundColor:color}} onClick={() =>setColors(color)}>{color}</button>
   )
 }
 
