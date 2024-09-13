157d1a7d06cc1e5d4413f7d509c467ef159d685e
paresh-2002 <pareshparesh.ay99099@gmail.com>
2024-09-13 14:23:10 +0530

    add style

--- 07reactRouter/src/components/User.jsx
+++ 07reactRouter/src/components/User.jsx
@@ -3,8 +3,8 @@ import { useParams } from 'react-router-dom'
 function User() {
     const {userId} = useParams()
   return (
-    <div className='bg-gray-600 text-white text-3xl p-4'>User: {userId}</div>
+    <div className='mx-auto w-full max-w-7xl bg-gray-600 text-white text-center text-3xl  p-4'>User: {userId}</div>
   )
 }
 
-export default User
+export default User;
